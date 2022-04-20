e05fa83c4ca12740f510e91d114ec890f1000d03
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2022-04-20 21:29:55 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+
+import requests
+
+def lambda_handler(event, context):
+    res = requests.get('https://google.com')
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "Requests status code": res.status_code,
+            "Requests body": res.text
+        })
+    }
+
+print(lambda_handler(None, None))
