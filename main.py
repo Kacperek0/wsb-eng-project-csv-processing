6579a3bdee04d1cc4caa5172ef3896d8e5fd08de
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2022-05-06 00:28:06 +0200

    fix encoding

--- main.py
+++ main.py
@@ -16,37 +16,43 @@ def parse_csv_mbank(lines):
     result = []
 
     for line in lines:
-         result.append({
-            'date': line[0],
-            'description': line[1],
-            'category': line[3],
-            'amount': line[4]
-        })
+        try:
+            line = line.decode('windows-1250')
+            line = line.split(';')
+            result.append({
+                'date': line[0],
+                'description': line[1],
+                'category': line[3],
+                'amount': line[4]
+            })
+        except IndexError:
+            continue
 
     return result
+
 
 def lambda_handler(event, context):
     """Receive file from S3 bucket, process it and return a JSON file to S3 bucket."""
     print(event)
 
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='windows-1250')
 
     try:
         response = s3.get_object(
             Bucket=bucket,
-            Key=key).splitlines()
+            Key=key)
 
-        lines = csv.reader(response)
-        next(lines)
+        lines = response['Body'].read().split(b'\n')
 
         result = parse_csv_mbank(lines)
 
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        print(f'Error getting object {key} from bucket {bucket}.')
         raise e
 
+    print(json.dumps(result))
 
     return {
         "statusCode": 200,
